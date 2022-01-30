22d5d54f11299cf70e4131eec2055d37bff2b01c
Marcelo Ratton <rattones@marceloratton.com>
2022-01-30 17:46:07 -0300

    migrations

---
+++ backend/database/migrations/2022_01_29_185735_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('fullname');
+            $table->string('username');
+            $table->string('password', 50);
+            $table->enum('role', [ 'user', 'admin' ])->default('user');
+            $table->timestamps();
+
+            $table->primary('id');
+            $table->unique('username');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
